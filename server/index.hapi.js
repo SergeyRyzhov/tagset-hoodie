ca2d05bd0f07196ac1b9ca57b5474c9a77b68110
Sergey Ryzhov <serega.rf@gmail.com>
2018-04-07 10:33:03 +0200

    test hapi server

---
+++ server/index.hapi.js
@@ -0,0 +1,40 @@
+var Hapi = require('hapi')
+// var hoodie = require('hoodie').register
+// var PouchDB = require('pouchdb-core')
+// .plugin(require('pouchdb-mapreduce'))
+// .plugin(require('pouchdb-adapter-memory'))
+
+var server = new Hapi.Server()
+server.connection({
+  host: 'localhost',
+  port: process.env.PORT
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, handler) {
+    handler.response('hello world')
+  }
+})
+
+// server.register({
+//   register: hoodie,
+//   options: { // pass options here
+//     inMemory: true,
+//     public: 'server/public',
+//     PouchDB: PouchDB
+//   }
+// }, function (error) {
+//   if (error) {
+//     throw error
+//   }
+// })
+
+server.start(function (error) {
+  if (error) {
+    throw error
+  }
+
+  console.log(('Server running at:', server.info.uri))
+})
